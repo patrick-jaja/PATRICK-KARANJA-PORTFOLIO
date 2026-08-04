fe3040958ffa488d93a5351a16760a2a6793fe60
PATRICK KARANJA <jajadezigns@gmail.com>
2026-08-04 13:38:12 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { readdirSync } from 'fs';
+
+const htmlFiles = readdirSync(__dirname).filter(file => file.endsWith('.html'));
+const input = {};
+htmlFiles.forEach(file => {
+  const name = file.replace('.html', '');
+  input[name] = resolve(__dirname, file);
+});
 
 export default defineConfig({
   root: '.',
   base: './',
   build: {
     outDir: 'dist',
-    rollupOptions: { input: 'index.html' },
+    rollupOptions: { input },
     assetsDir: 'assets',
   },
   server: {
